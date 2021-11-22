186417960b2a042bf3487136e0d2cedf97161258
Christel Carbajal <chrdeveloper02@gmail.com>
2021-11-22 16:58:52 +0100

    updated createEmoji function

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const heartButton = document.querySelector("#heart");
+const emoji = document.querySelector("#emoji");
+heartButton.addEventListener("click", () => lotsOfEmoji());
+emojiArray = ["❤️‍🔥️", "🌟", "💯", "💥"];
+
+function lotsOfEmoji() {
+  for (let i = 0; i < 20; i++) {
+    createEmoji();
+  }
+}
+
+function createEmoji() {
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+  div.innerHTML = emojiArray[Math.floor(Math.random() * 3)];
+  div.style.width = Math.random() * 600 + "px";
+  div.classList.add("emoji");
+  div.style.animationDelay = `${Math.random()}s`;
+  div.classList.add("moving");
+  // remove the div at the end of the animation!
+  div.addEventListener("animationend", (e) => e.target.remove());
+}
